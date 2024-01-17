3cbd1fee89325066caa221c7f05ea9f7614738d4
bitmole <robert@idea-loop.com>
2024-01-17 16:18:31 -0700

    User input to calculator, table, fix overpayment.

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -2,18 +2,28 @@
 #
 # Exercise 1.7
 
-principal = 500000.0
-rate = 0.05
-payment = 2684.11
-extra_payment = 1000
-npayments = 0
-total_paid = 0.0
+def calc(start, end, extra_payment):
+    principal = 500000.0
+    rate = 0.05
+    payment = 2684.11
+    npayments = 0
+    total_paid = 0.0
 
-while principal > 0:
-    npayments = npayments + 1
-    cur_payment = payment + extra_payment if npayments <= 12 else payment
-    principal = principal * (1+rate/12) - cur_payment 
-    total_paid = total_paid + cur_payment
-    print(npayments, round(cur_payment, 2), round(total_paid), round(principal, 2))
+    while principal > 0:
+        npayments = npayments + 1
+        cur_payment = payment + extra_payment if start <= npayments <= end else payment
+        principal = principal * (1+rate/12) - cur_payment 
+        total_paid = total_paid + cur_payment
+        print(npayments, round(cur_payment, 2), round(total_paid, 2), round(principal, 2))
 
-print('Total paid', round(total_paid, 2), "in", npayments, "months")
+    if principal < 0: # overpaid?
+        total_paid -= -principal
+
+    print('Total paid', round(total_paid, 2), "in", npayments, "months")
+
+if __name__ == "__main__":
+    start = input('start month: ')
+    end = input('end month: ')
+    extra_payment = input('extra payment: ')
+    args = [int(s) for s in (start, end, extra_payment)]
+    calc(*args)
