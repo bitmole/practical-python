dc6e2141df8c3d1068c821512cb11468bfac5b7a
bitmole <robert@idea-loop.com>
2024-01-17 14:46:18 -0700

    Complete to spec.

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+def main():
+    cur_height = 100
+
+    for bounce in range(10):
+        cur_height *= 3/5
+        print(bounce+1, round(cur_height, 4))
+
+if __name__ == "__main__":
+    main()
