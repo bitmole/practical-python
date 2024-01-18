26130b1eeee609dc586730efc0d7a5eb530b4950
bitmole <robert@idea-loop.com>
2024-01-18 10:59:56 -0700

    Sum portfolio total.

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,4 +3,12 @@
 # Exercise 1.27
 
 def main():
-    pass
+    total = 0
+    f = open('./Data/portfolio.csv', 'rt')
+    _ = next(f) # skip headers
+    for line in f:
+        stock, shares, price = line.split(',')
+        shares, price = int(shares), float(price)
+        total += shares * price
+    f.close()
+    return f'Total cost {total:0.2f}'
