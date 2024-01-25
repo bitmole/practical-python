b16dcd742f08c377a660d20bbc75f0cd5a00a227
bitmole <robert@idea-loop.com>
2024-01-25 10:42:39 -0700

    Parameterize type conversion.

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,7 +3,7 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename, select=None):
+def parse_csv(filename, select=None, types=None):
     """Parse a CSV file into a list of records
        structured as dicts.
 
@@ -11,16 +11,27 @@ def parse_csv(filename, select=None):
     :returns: list of records (dicts)
 
     """
+    records = []
     with open(filename) as f:
         rows = csv.reader(f)
         headers = next(rows)
 
+        if select and types:
+            assert len(select)==len(types)
+
+        rows = (r for r in rows if r) # ignore empty rows
+
+        # filter columns
         if select:
             indices = [headers.index(c) for c in select]
             headers = select
-            # filter & sanitize
-            rows = [[row[i] for i in indices] for row in rows if row]
+            rows = [[row[i] for i in indices] for row in rows]
 
+        # convert
+        if types:
+            rows = [[t(val) for t, val in zip(types, row)] for row in rows]
+
+        # package to records
         records = [dict(zip(headers, row)) for row in rows] 
 
     return records
