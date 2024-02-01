8801972685a3c0b6dfafb5817349a4d26a5f86bf
bitmole <robert@idea-loop.com>
2024-02-01 10:46:23 -0700

    Properties, setters, slots.

--- Work/stock.py
+++ Work/stock.py
@@ -1,5 +1,7 @@
 class Stock(object):
     """Represents stock holding."""
+    __slots__ = ('name', '_shares', 'price')
+
     def __init__(self, name, shares, price):
         self.name, self.shares, self.price = name, shares, price
 
@@ -15,6 +17,16 @@ class Stock(object):
     @property
     def cost(self):
         return self.shares * self.price
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('expected an integer')
+        self._shares = value
 
     def sell(self, n):
         assert n <= self.shares
