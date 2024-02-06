74b051f489e9e954ee2ee16553fcca28115ca01d
bitmole <robert@idea-loop.com>
2024-02-06 16:22:49 -0700

    Closures, typed properties

--- Work/stock.py
+++ Work/stock.py
@@ -1,9 +1,17 @@
+from typedproperty import String, Integer, Float
+
 class Stock(object):
     """Represents stock holding."""
-    __slots__ = ('name', '_shares', 'price')
+    __slots__ = ('_name', '_shares', '_price')
+    name = String('name')
+    shares = Integer('shares')
+    price = Float('price')
+
 
     def __init__(self, name, shares, price):
-        self.name, self.shares, self.price = name, shares, price
+        self.name = name
+        self.shares = shares
+        self.price = price
 
     def __repr__(self):
         return f"Stock('{self.name}', {self.shares}, {self.price})"
@@ -17,16 +25,6 @@ class Stock(object):
     @property
     def cost(self):
         return self.shares * self.price
-
-    @property
-    def shares(self):
-        return self._shares
-
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, int):
-            raise TypeError('expected an integer')
-        self._shares = value
 
     def sell(self, n):
         assert n <= self.shares
